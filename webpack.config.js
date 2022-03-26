5a4cde11a33427061f56314a87479f71dc6a29c7
Aditya Patange <adipat247360@gmail.com>
2022-03-26 13:07:44 +0530

    webpack config: add eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable quotes */
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+const ESLintPlugin = require("eslint-webpack-plugin");
+
 module.exports = {
   entry: "./src/js/main.js",
   output: {
